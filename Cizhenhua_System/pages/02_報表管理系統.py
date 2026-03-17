8fe3787cf9a5f16b431436a4fa52c1302dbfc5fb
佰哥資訊開發股份有限公 <138289032+generation111@users.noreply.github.com>
2026-03-18 05:48:41 +0800

    Update 02_報表管理系統.py

--- Cizhenhua_System/pages/02_報表管理系統.py
+++ Cizhenhua_System/pages/02_報表管理系統.py
@@ -37,12 +37,12 @@ st.markdown("""
     
    /* 訪談內容錄入框：高度再調降 50% 並優化視覺 */
 div[data-baseweb="textarea"] { 
-    min-height: 10px !important;  /
+    min-height: 8px !important;  /
 }
 
 div[data-baseweb="textarea"] textarea {
     padding: 4px 8px !important;  /* 縮減內邊距，騰出空間 */
-    line-height: 2!important;   /* 調整行高，讓單行文字置中 */
+    line-height: 1!important;   /* 調整行高，讓單行文字置中 */
     font-size: 14px !important;
 }
 
