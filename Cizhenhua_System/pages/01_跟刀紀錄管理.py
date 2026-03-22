32530b331155161cc3589164973f587296875ff2
佰哥資訊開發股份有限公 <138289032+generation111@users.noreply.github.com>
2026-03-22 14:58:19 +0800

    Update 01_跟刀紀錄管理.py

--- Cizhenhua_System/pages/01_跟刀紀錄管理.py
+++ Cizhenhua_System/pages/01_跟刀紀錄管理.py
@@ -28,7 +28,7 @@ st.markdown(f"""
         background-color: white !important;
         border: 1px solid #1e3a8a !important;
         border-radius: 8px !important;
-        height: 48px !important;
+        height: 44px !important;
         box-sizing: border-box !important;
     }}
 
